4621eacdca441cbdc5fae44e2e7030958ef7b17f
LyesOrd <lyes.ord@gmail.com>
2023-07-07 11:25:24 +0200

    feat: new establishment

---
+++ src/components/EstablishmentList.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { EstablishmentInfo } from './data/establishment';
+import { Container } from './styles/molecules';
+import { EstablishmentSection, Section } from './styles/molecules/Section';
+import { H1, P } from './styles/atoms';
+import { EstablishmentImg } from './styles/atoms/Image';
+
+function EtablissementsListe() {
+  const [etablissements, setEtablissements] = useState<EstablishmentInfo[]>([]);
+
+  useEffect(() => {
+    // Effectuer la requête API pour récupérer les établissements
+    // Assurez-vous d'utiliser l'URL et les paramètres appropriés pour filtrer par type d'établissement (ex : salons de coiffure)
+
+    // Exemple d'appel à l'API avec fetch :
+    fetch('http://localhost:8080/establishments/')
+      .then(response => response.json())
+      .then(data => setEtablissements(data));
+  }, []);
+
+  return (
+    <>
+            {etablissements.map((etablissement, index) => {
+                return (
+                    <EstablishmentSection color="#FFF">
+                        <H1>{etablissement.name}</H1>
+                        <P>{etablissement.description}</P>
+                        
+                    </EstablishmentSection>
+                )
+                }
+            )
+            }
+    </>
+    
+  );
+}
+
+export default EtablissementsListe;
---
+++ src/components/data/establishment.ts
@@ -0,0 +1,12 @@
+
+export interface EstablishmentInfo {
+    "id": 1,
+    "siret": string,
+    "name": string,
+    "description": string
+    "street_number": string,
+    "street_name": string,
+    "adress_description": string,
+    "postal_code": number,
+    "city": string
+  }
