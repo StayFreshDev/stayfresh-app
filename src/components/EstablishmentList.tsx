eac1dccdc4d16d73352e680864b6e58e9d9c675c
raparisonbryan <raparisonbryan@yahoo.fr>
2023-07-18 09:14:46 +0200

    add search bar

--- src/components/EstablishmentList.tsx
+++ src/components/EstablishmentList.tsx
@@ -1,53 +1,65 @@
 import { useEffect, useState } from 'react';
-import { EstablishmentInfo } from './data/establishment';
 import { ButtonDiv, EstablishmentDivGlobal, EstablishmentInfoDiv, EstablishmentSection, ImageEstablishmentDiv, InfoEstablishment } from './styles/molecules/Section';
-import { Button, H1, H2, P } from './styles/atoms';
+import { Button, H2, P } from './styles/atoms';
+import { useLocation } from 'react-router-dom';
+
+interface Establishment {
+  id: number;
+  establishment_name: string;
+  establishment_description: string;
+  establishment_address: string;
+}
 
 function EtablissementsListe() {
-  const [etablissements, setEtablissements] = useState<EstablishmentInfo[]>([]);
+  const [results, setResults] = useState<Establishment[]>([]);
+  const location = useLocation();
 
   useEffect(() => {
-    fetch('http://51.254.118.50/establishments/')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setEtablissements(data);
-      })
-  }, []);
+    // Extrait la valeur de recherche de l'URL
+    const params = new URLSearchParams(location.search);
+    const searchValue = params.get('term');
 
-  const salonType = "Coiffure";
+    // Effectue la recherche / le filtrage
+    fetch(`http://51.254.118.50/establishments`)
+        .then(res => res.json())
+        .then(data => {
+            // Si un terme de recherche est présent, filtrez les résultats
+            if (searchValue) {
+                const filteredResults = data.filter((establishment: Establishment) =>
+                    establishment.establishment_name.includes(searchValue));
+                setResults(filteredResults);
+            }
+            // Sinon, affichez tous les établissements
+            else {
+                setResults(data);
+            }
+        })
+        .catch(err => console.log(err));
+}, [location]);
 
   return (
     <>
           <EstablishmentDivGlobal>
-          <H1>Salon de {salonType} :</H1>
-            {etablissements.map((etablissement, index) => {
-              {console.log(etablissement.establishment_address);}
-                return (
-                  <>
-                    <EstablishmentSection key={index} color="#F4F4F4">
+          {results.map(result => (
+                <div key={result.id}>
+                    <EstablishmentSection color="#F4F4F4">
                       <EstablishmentInfoDiv>
                         <ImageEstablishmentDiv>
                           {/* <img src={etablissement.image} alt={etablissement.name} /> */}
                         </ImageEstablishmentDiv>
                         <InfoEstablishment>
-                          <H2>{etablissement.establishment_name}</H2>
-                          <P>{etablissement.establishment_description}</P>
-                          <P>{etablissement.establishment_address}</P>
+                          <H2>{result.establishment_name}</H2>
+                          <P>{result.establishment_description}</P>
+                          <P>{result.establishment_address}</P>
                         </InfoEstablishment>
                       </EstablishmentInfoDiv>
                       <ButtonDiv>
                         <Button customAttribute='' color="#546A7B">Prendre rendez-vous</Button>
                       </ButtonDiv>
                     </EstablishmentSection>
-                  </>
-                  )
-                }
-              )
-            }
-          </EstablishmentDivGlobal>
-            
-            
+                </div>
+            ))}
+          </EstablishmentDivGlobal>      
     </>
     
   );
